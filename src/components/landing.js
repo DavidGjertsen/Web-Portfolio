bacfc0d0179c46df6c17b8f1a230418533b4d9b2
David Gjertsen <davidgjertsen8635@gmail.com>
2021-03-16 13:11:09 -0400

    Scrolling prompt works now. Yay.

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,3 +1,4 @@
+import {useState, useEffect} from "react";
 import arrowDown from "../images/icons/arrow-down.png";
 
 export const LandingPage = () => {
@@ -12,13 +13,27 @@ export const LandingPage = () => {
 }
 
 const PageHeader = () => {
+    const [active, setActive] = useState(true);
+
+    useEffect(() => {
+        const onScroll = () => {
+            setActive(false);
+        }
+
+        document.getElementsByClassName("container")[0].addEventListener("scroll", onScroll);
+
+        return function cleanup() {
+            document.getElementsByClassName("container")[0].removeEventListener("scroll", onScroll);
+        }
+    });
+
     return(
         <section className="panel">
             <div className = "pageHeaderContent">
                 <h1>David Gjertsen</h1>
                 <p>I am a digital creator and front-end web designer.</p>
                 <p>Let me show you what I can do.</p>
-                <img id="scrollPrompt" src={arrowDown} alt="Scroll down for more" className="hidden"/>
+                <img id="scrollPrompt" src={arrowDown} alt="Scroll down for more" className={active ? "visible" : "hidden"}/>
             </div>
         </section>
     )
